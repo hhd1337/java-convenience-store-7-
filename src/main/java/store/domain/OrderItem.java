bb5d77cab5a67d62c3f09cbb8d0a46bc3018e0af
goeka <goeka1337@gmail.com>
2025-12-22 05:59:03 +0900

    feat(domain): OrderItem 클래스 추가

---
+++ src/main/java/store/domain/OrderItem.java
@@ -0,0 +1,11 @@
+package store.domain;
+
+public class OrderItem {
+    private String name;
+    private int quantity;
+
+    public OrderItem(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}

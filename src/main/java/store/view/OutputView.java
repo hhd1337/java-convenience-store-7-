163df7448097df8a79a60ec93d0a9f6cc3cba673
goeka <goeka1337@gmail.com>
2025-12-24 16:05:12 +0900

    fix(view): 영수증 출력 포맷에 맞게 수정
    - 각 라인별 요소들 \t 적용하여 정렬함
    - 금액 포맷에 맞게 천 단위에 "," 붙이도록 함

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,9 +1,12 @@
 package store.view;
 
+import java.text.NumberFormat;
 import java.util.List;
 import store.dto.ReceiptDto;
 
 public class OutputView {
+    private static final NumberFormat MONEY = NumberFormat.getInstance();
+
     public void printStock(List<String> lines) {
         System.out.println("안녕하세요. w편의점입니다.");
         System.out.println("현재 보유하고 있는 상품입니다.\n");
@@ -30,19 +33,24 @@ public class OutputView {
         System.out.println("===============w 편의점==============");
         System.out.println("상품명\t\t수량\t금액");
         for (ReceiptDto.PurchaseLine line : receipt.purchases()) {
-            System.out.println(line.name() + line.quantity() + line.amount());
+            System.out.println(line.name() + "\t\t" + line.quantity() + "\t" + formatNumberWithComma(line.amount()));
         }
 
         System.out.println("================증 정================");
         for (ReceiptDto.GiftLine line : receipt.gifts()) {
-            System.out.println(line.name() + line.quantity());
+            System.out.println(line.name() + "\t\t" + line.quantity());
         }
 
         System.out.println("====================================");
-        System.out.println(
-                "총 구매액 " + receipt.summary().totalItemQuantity() + receipt.summary().totalAmountBeforeDiscount());
-        System.out.println("행사할인 " + "-" + receipt.summary().promotionDiscount());
-        System.out.println("멤버십할인" + "-" + receipt.summary().membershipDiscount());
-        System.out.println("내실 돈" + receipt.summary().finalPayAmount());
+        System.out.println("총 구매액" + "\t" + receipt.summary().totalItemQuantity() + "\t" + formatNumberWithComma(
+                receipt.summary().totalAmountBeforeDiscount()));
+        System.out.println("행사할인" + "\t\t\t" + "-" + formatNumberWithComma(receipt.summary().promotionDiscount()));
+        System.out.println("멤버십할인" + "\t\t" + "-" + formatNumberWithComma(receipt.summary().membershipDiscount()));
+        System.out.println("내실돈" + "\t\t\t" + formatNumberWithComma(receipt.summary().finalPayAmount()));
     }
+
+    private String formatNumberWithComma(int amount) {
+        return MONEY.format(amount);
+    }
+
 }

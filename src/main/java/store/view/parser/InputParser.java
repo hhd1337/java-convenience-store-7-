a6346898f04ba375dac484da35c450c205607382
goeka <goeka1337@gmail.com>
2025-12-27 07:22:47 +0900

    refactor(view): [이름-수량] 파싱 로직에서 ","와 "-" 상수화

--- src/main/java/store/view/parser/InputParser.java
+++ src/main/java/store/view/parser/InputParser.java
@@ -8,21 +8,22 @@ import store.domain.order.OrderItem;
 import store.util.ErrorMessage;
 
 public class InputParser {
+    private static final String ITEM_DELIMITER = ",";
+    private static final String NAME_QUANTITY_DELIMITER = "-";
+
     public static List<OrderItem> parseOrderItems(String input) {
         Set<String> seenNames = new HashSet<>();
 
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(ITEM_DELIMITER))
                 .map(String::trim)
                 .map(s -> s.replace("[", "").replace("]", "")) // 사이다-2
-                .map(token -> token.split("-"))
+                .map(token -> token.split(NAME_QUANTITY_DELIMITER))
                 .map(parts -> {
                     String name = parts[0].trim();
                     int quantity = Integer.parseInt(parts[1].trim());
 
                     if (!seenNames.add(name)) {
-                        throw new IllegalArgumentException(
-                                ErrorMessage.PREFIX + "같은 상품은 합쳐서 한번만 입력해주세요: " + name
-                        );
+                        throw new IllegalArgumentException(ErrorMessage.PREFIX + "같은 상품은 합쳐서 한번만 입력해주세요: " + name);
                     }
 
                     OrderItem orderItem = new OrderItem(name, quantity);

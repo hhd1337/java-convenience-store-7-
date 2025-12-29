56e8df1a83771953b0a7c2a05e3ef56153323c94
goeka <goeka1337@gmail.com>
2025-12-30 03:29:59 +0900

    style(domain): 재고수량 초과 주문 시 에러메시지 요구사항에 맞게 수정

--- src/main/java/store/domain/stock/Stock.java
+++ src/main/java/store/domain/stock/Stock.java
@@ -20,7 +20,7 @@ public class Stock {
 
     public void validateEnoughStock(String name, int quantity) {
         if (quantity > findQuantityByName(name)) {
-            throw new IllegalArgumentException(ErrorMessage.PREFIX + name + "상품 재고가 부족합니다. 다시 입력해주세요.");
+            throw new IllegalArgumentException(ErrorMessage.PREFIX + " 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
         }
     }
 
